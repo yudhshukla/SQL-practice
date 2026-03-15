38fe62131409eeeb8a4c299ed384e2134bac4db9
yshukla0 <150735799+yshukla0@users.noreply.github.com>
2026-03-14 21:41:15 -0400

    Ideating table structure

---
+++ flagSorter.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+#database to store flag colors, size, country, continent, symbols (weapons, people, animals, nature), year created
+
+'''
+-----------------------------------------------------------------
+id | country | continent | colors | size | symbols | year created
+-----------------------------------------------------------------
+'''
+
+#id : INTEGER, country : TEXT, continent : TEXT, colors : TEXT, size : TEXT, symbols : TEXT, year created : INTEGER
+
